5d8ef87a987b29f9adfb5b6a7caf955d1570d498
Ikebe, Ryohji <Ryohji.Ikebe@gmail.com>
2018-09-14 01:15:23 +0900

    Refine text.c.

--- src/text.c
+++ src/text.c
@@ -4,46 +4,44 @@
 #include <stdlib.h>
 #include <string.h>
 
+static char* text_car(const struct sexp sexp);
 static char* text_cdr(const struct sexp sexp);
+static char* text_pair(const struct sexp sexp, char prefix);
 
 char* text(const struct sexp sexp) {
+  return text_car(sexp);
+}
+
+char* text_car(const struct sexp sexp) {
   if (atom(sexp)) {
-    if (nil(sexp)) {
-      return strdup("()");
-    } else {
-      return strdup(sexp.p);
-    }
+    const char* const p = nil(sexp) ? "()" : sexp.p;
+    return strdup(p);
   } else {
-    char* const car = text(fst(sexp));
-    char* const cdr = text_cdr(snd(sexp));
-    char* const buf = malloc(strlen(car) + strlen(cdr) + 2);
-    sprintf(buf, "(%s%s", car, cdr);
-    free(car);
-    free(cdr);
-    return buf;
+    return text_pair(sexp, '(');
   }
 }
 
 char* text_cdr(const struct sexp sexp) {
   if (atom(sexp)) {
-    if (nil(sexp)) {
-      return strdup(")");
-    } else {
-      char* const buf = malloc(strlen(sexp.p) + 5);
-      sprintf(buf, " . %s)", sexp.p);
-      return buf;
-    }
-  } else {
-    char* const car = text(fst(sexp));
-    char* const cdr = text_cdr(snd(sexp));
-    char* const buf = malloc(strlen(car) + strlen(cdr) + 2);
-    sprintf(buf, " %s%s", car, cdr);
-    free(car);
-    free(cdr);
+    const char* const p = nil(sexp) ? "" : ": ";
+    const char* const q = nil(sexp) ? "" : sexp.p;
+    char* buf = malloc(strlen(p) + strlen(q) + 2);
+    sprintf(buf, "%s%s)", p, q);
     return buf;
+  } else {
+    return text_pair(sexp, ' ');
   }
 }
 
+inline char* text_pair(const struct sexp sexp, char prefix) {
+  char* const car = text_car(fst(sexp));
+  char* const cdr = text_cdr(snd(sexp));
+  char* const buf = malloc(strlen(car) + strlen(cdr) + 2);
+  sprintf(buf, "%c%s%s", prefix, car, cdr);
+  free(car);
+  free(cdr);
+  return buf;
+}
 
 #ifdef UNITTEST_
 /* NEED data.o without UNITTEST_. */
@@ -59,16 +57,20 @@ int main() {
   if (strcmp("hello", str)) return 2;
   free(str);
 
-  str = text(cons(symbol("hello"), symbol("world")));
-  if (strcmp("(hello . world)", str)) return 3;
+  str = text(cons(symbol("about"), symbol("blank")));
+  if (strcmp("(about: blank)", str)) return 3;
+  free(str);
+
+  str = text(cons(symbol("hello"), cons(symbol("world"), NIL())));
+  if (strcmp("(hello world)", str)) return 4;
   free(str);
 
   str = text(cons(symbol("ulisp"), NIL()));
-  if (strcmp("(ulisp)", str)) return 4;
+  if (strcmp("(ulisp)", str)) return 5;
   free(str);
 
   str = text(cons(cons(symbol("x"), symbol("1")), cons(cons(symbol("y"), symbol("2")), NIL())));
-  if (strcmp("((x . 1) (y . 2))", str)) return 5;
+  if (strcmp("((x: 1) (y: 2))", str)) return 6;
   free(str);
 
   return 0;
