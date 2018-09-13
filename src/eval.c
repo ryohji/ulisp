8faf712345308fe2965838475ee47a0a69a9c589
Ikebe, Ryohji <Ryohji.Ikebe@gmail.com>
2018-09-14 05:20:31 +0900

    Implement symbol evaluation (not found in environment.)

--- src/eval.c
+++ src/eval.c
@@ -1,13 +1,37 @@
 #include "ulisp.h"
 
+#include <setjmp.h>
+
+static jmp_buf CALL_CONTEXT;
+
+#define trap()       setjmp(CALL_CONTEXT)
+#define throw(cause) longjmp(CALL_CONTEXT, (cause))
+
 struct sexp eval(const struct sexp exp, const struct sexp env) {
-  return NIL();
+  if (atom(exp)) {
+    if (!nil(exp)) {
+      throw(1);
+    }
+    return NIL();
+  } else {
+    return NIL();
+  }
 }
 
 #ifdef UNITTEST_
 int main() {
   if (!nil(eval(NIL(), NIL()))) {
     return 1;
+  }
+
+  switch (trap()) {
+  case 0:
+    while (true) {
+      eval(symbol("t"), NIL());
+    }
+    break;
+  default:
+    break;
   }
 
   return 0;
