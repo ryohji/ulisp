69abb2d79038dc8a87ba8fd6495c8762376bb04a
Ikebe, Ryohji <Ryohji.Ikebe@gmail.com>
2018-09-15 02:08:06 +0900

    Add symbol not found message.

--- src/eval.c
+++ src/eval.c
@@ -1,11 +1,14 @@
 #include "ulisp.h"
 
 #include <setjmp.h>
-#include <stdbool.h>
+#include <stdio.h>
+
+static const char* Err_value_not_found = "Value for symbol `%s` not found.\n";
 
 struct sexp eval(jmp_buf trap, const struct sexp exp, const struct sexp env) {
   if (atom(exp)) {
     if (!nil(exp)) {
+      fprintf(stderr, Err_value_not_found, exp.p);
       longjmp(trap, 1);
     }
     return NIL();
@@ -15,6 +18,9 @@ struct sexp eval(jmp_buf trap, const struct sexp exp, const struct sexp env) {
 }
 
 #ifdef UNITTEST_
+#include <stdlib.h>
+#include <string.h>
+
 int main() {
   jmp_buf trap;
 
@@ -22,14 +28,29 @@ int main() {
     return 1;
   }
 
-  switch (setjmp(trap)) {
-  default:
-    while(true) {
-      eval(trap, symbol("t"), NIL());
+  {
+    FILE* t = stderr;
+    char* p;
+    size_t n;
+    stderr = open_memstream(&p, &n);
+
+    switch (setjmp(trap)) {
+    default:
+      while(true) {
+	eval(trap, symbol("t"), NIL());
+      }
+      break;
+    case 1:
+      fflush(stderr);
+      if (strcmp("Value for symbol `t` not found.\n", p)) {
+	return 2;
+      }
+      break;
     }
-    break;
-  case 1:
-    break;
+
+    free(p);
+    fclose(stderr);
+    stderr = t;
   }
 
   return 0;
