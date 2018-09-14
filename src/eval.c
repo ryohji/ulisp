4f0f8417421e53001ae52773be4ea41ea61b57ed
Ikebe, Ryohji <Ryohji.Ikebe@gmail.com>
2018-09-14 23:57:23 +0900

    Change to pass eval jmp_buf directly.

--- src/eval.c
+++ src/eval.c
@@ -1,16 +1,12 @@
 #include "ulisp.h"
 
 #include <setjmp.h>
+#include <stdbool.h>
 
-static jmp_buf CALL_CONTEXT;
-
-#define trap()       setjmp(CALL_CONTEXT)
-#define throw(cause) longjmp(CALL_CONTEXT, (cause))
-
-struct sexp eval(const struct sexp exp, const struct sexp env) {
+struct sexp eval(jmp_buf trap, const struct sexp exp, const struct sexp env) {
   if (atom(exp)) {
     if (!nil(exp)) {
-      throw(1);
+      longjmp(trap, 1);
     }
     return NIL();
   } else {
@@ -20,17 +16,19 @@ struct sexp eval(const struct sexp exp, const struct sexp env) {
 
 #ifdef UNITTEST_
 int main() {
-  if (!nil(eval(NIL(), NIL()))) {
+  jmp_buf trap;
+
+  if (!nil(eval(trap, NIL(), NIL()))) {
     return 1;
   }
 
-  switch (trap()) {
-  case 0:
-    while (true) {
-      eval(symbol("t"), NIL());
+  switch (setjmp(trap)) {
+  default:
+    while(true) {
+      eval(trap, symbol("t"), NIL());
     }
     break;
-  default:
+  case 1:
     break;
   }
 
