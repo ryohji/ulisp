7ef6a502838d6edc77526cc64beab85f9d66bac8
Ikebe, Ryohji <Ryohji.Ikebe@gmail.com>
2018-09-18 01:04:34 +0900

    Add interface definition to `uisp.h`.

--- src/ulisp.h
+++ src/ulisp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdbool.h>
+#include <setjmp.h>
 
 struct sexp {
   const bool symbol;
@@ -41,6 +42,17 @@ struct sexp fst(const struct sexp sexp);
  * Return `cdr` of sexp.
  */
 struct sexp snd(const struct sexp sexp);
+
+/**
+ * Evaluate expression on the environment.
+ * 
+ * @param trap is a execution state of host. \
+ * If error condition are met, rewind to trapped state by longjmp. \
+ * Error message are flushed to stderr.
+ * @param env_exp is a pair of (environment: expression).
+ * @return S-expression which is a pair of resulting environment and evaluated value.
+ */
+struct sexp eval(jmp_buf trap, const struct sexp env_exp);
 
 /**
  * Build text representation of sexp.
