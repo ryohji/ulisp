c466e81587df84f321b1eea9ec0e7cbc8f8152d9
Ikebe, Ryohji <Ryohji.Ikebe@gmail.com>
2018-09-21 01:40:09 +0900

    Add REPL implementation in main.c, and Makefile.

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include "ulisp.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <setjmp.h>
+
+static void repl(jmp_buf trap, struct sexp env_exp);
+
+int main() {
+    jmp_buf trap;
+    struct sexp env = cons(cons(symbol("t"), symbol("True")), NIL());
+loop:
+    printf("> ");
+    switch (setjmp(trap)) {
+    case TRAP_NONE:
+        fflush(stdout);
+        repl(trap, cons(env, read(trap)));
+    case TRAP_NOINPUT:
+        return 0;
+    }
+    goto loop;
+}
+
+static void repl(jmp_buf trap, struct sexp env_exp) {
+    struct sexp r = eval(trap, env_exp);
+    char* p = text(snd(r));
+    printf("%s\n> ", p);
+    fflush(stdout);
+    free(p);
+    repl(trap, cons(fst(r), read(trap)));
+}
