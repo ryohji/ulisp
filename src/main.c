1379b818a12d3cb23e9acf9ee5109c5d947dd9c6
Ikebe, Ryohji <Ryohji.Ikebe@gmail.com>
2018-09-29 15:21:46 +0900

    Inline `repl`.

--- src/main.c
+++ src/main.c
@@ -6,32 +6,25 @@
 
 extern bool freadable(FILE* fp);
 
-static void repl(jmp_buf trap, const struct env_exp env_exp);
-
 int main() {
     jmp_buf trap;
     const struct sexp* env = cons(cons(symbol("t"), symbol("True")), NIL());
+    struct env_exp r = { .env = env, };
+    char* p = 0;
 loop:
-    if (!freadable(stdin))
-        printf("> ");
     switch (setjmp(trap)) {
     case TRAP_NONE:
-        fflush(stdout);
-        repl(trap, (struct env_exp){ env, read(trap) });
+        if (!freadable(stdin)) {
+            printf("> ");
+            fflush(stdout);
+        }
+        r = eval(trap, (struct env_exp){ r.env, read(trap) });
+        char* p = text(r.exp);
+        puts(p);
+        free(p);
+        break;
     case TRAP_NOINPUT:
         return 0;
     }
     goto loop;
-}
-
-static void repl(jmp_buf trap, const struct env_exp env_exp) {
-    const struct env_exp r = eval(trap, env_exp);
-    char* p = text(r.exp);
-    printf("%s\n", p);
-    free(p);
-    if (!freadable(stdin)) {
-        printf("> ", p);
-    }
-    fflush(stdout);
-    repl(trap, (struct env_exp){ r.env, read(trap) });
 }
