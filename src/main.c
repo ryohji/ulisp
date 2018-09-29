7a0c1a0791b77fe8367052317ad989f1cc3f6921
Ikebe, Ryohji <Ryohji.Ikebe@gmail.com>
2018-09-29 15:25:28 +0900

    Ommit goto loop.

--- src/main.c
+++ src/main.c
@@ -10,21 +10,22 @@ int main() {
     jmp_buf trap;
     const struct sexp* env = cons(cons(symbol("t"), symbol("True")), NIL());
     struct env_exp r = { .env = env, };
-    char* p = 0;
-loop:
+
     switch (setjmp(trap)) {
     case TRAP_NONE:
-        if (!freadable(stdin)) {
-            printf("> ");
-            fflush(stdout);
+        while (true) {
+            char* p;
+            if (!freadable(stdin)) {
+                printf("> ");
+                fflush(stdout);
+            }
+            r = eval(trap, (struct env_exp){ r.env, read(trap) });
+            p = text(r.exp);
+            puts(p);
+            free(p);
         }
-        r = eval(trap, (struct env_exp){ r.env, read(trap) });
-        char* p = text(r.exp);
-        puts(p);
-        free(p);
         break;
     case TRAP_NOINPUT:
         return 0;
     }
-    goto loop;
 }
