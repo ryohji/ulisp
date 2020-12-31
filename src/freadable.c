c2374e9b6128e817158d6d8fdb501addc049d737
Ikebe, Ryohji <Ryohji.Ikebe@gmail.com>
2020-12-31 23:45:21 +0900

    Bugfix: use `fp` to check readable

--- src/freadable.c
+++ src/freadable.c
@@ -1,14 +1,14 @@
 #include <stdbool.h>
 #include <stdio.h>
-#include <sys/types.h>
 #include <sys/time.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 bool freadable(FILE* fp) {
     fd_set fds;
     struct timeval timeout = {0};
     FD_ZERO(&fds);
-    FD_SET(fileno(stdin), &fds);
-    
-    return select(fileno(stdin) + 1, &fds, NULL, NULL, &timeout);
+    FD_SET(fileno(fp), &fds);
+
+    return select(fileno(fp) + 1, &fds, NULL, NULL, &timeout);
 }
